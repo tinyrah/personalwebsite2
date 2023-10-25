5460adf78894a127746273e8521eba9c8712205b
Raul Hernandez <raulhernandezwork@gmail.com>
2023-10-25 10:10:11 -0500

    functionality for resume

--- components/ResumeSection.tsx
+++ components/ResumeSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const ResumeSection: React.FC = () => {
   return (
-    <section className="mt-16">
+    <section id="resume" className="mt-16">  {/* id attribute added */}
       <h2 className="text-2xl font-semibold mb-4">Resume</h2>
       <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
         Click here to view my resume
