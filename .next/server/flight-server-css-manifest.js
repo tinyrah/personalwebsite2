ab4606b34146ed713768fa5d6e6425dc0eeb8bcf
Raul Hernandez <raulhernandezwork@gmail.com>
2023-10-25 07:25:01 -0500

    initial commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/raulhernandez/Desktop/personalwebsite2/app/page": [
+      "/Users/raulhernandez/Desktop/personalwebsite2/styles/globals.css"
+    ]
+  },
+  "/Users/raulhernandez/Desktop/personalwebsite2/app/layout.tsx": [
+    "/Users/raulhernandez/Desktop/personalwebsite2/styles/globals.css"
+  ]
+}
---
+++ components/AboutSection.tsx
@@ -0,0 +1,79 @@
+import React from "react"
+import Image from "next/image"
+
+const skills = [
+  { skill: "HTML" },
+  { skill: "CSS" },
+  { skill: "JavaScript" },
+  { skill: "Python" },
+  { skill: "React" },
+  { skill: "Next.js" },
+  { skill: "Git" },
+  { skill: "GitHub" },
+  { skill: "Java" },
+  { skill: "C++" },
+  { skill: "Firebase"},
+  { skill: "MySQL"},
+]
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">
+          About Me
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2 ">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Who am I?
+            </h1>
+            <p>
+              Hello, my name is Raul and I'm a computer science student at the University of Texas at Dallas.
+              I'd describe myself as a highly curious and self-motivated individual. Some of my favorite things to do (besides code) 
+              is going to the gym and listening to music. I'm based out of Dallas, TX but am always open to exploring new areas!
+            </p>
+            <br />
+            <p>
+              I am interested in software engineering, distributed systems, and low-level programming!
+            </p>
+            <br />
+            <p>
+              
+            </p>
+            <br />
+            <p>
+              
+            </p>
+          </div>
+          <div className="text-center md:w-1/2 md:text-left">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+              {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image
+              src="/hero-image.png"
+              alt=""
+              width={325}
+              height={325}
+              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
